cad1edb20fa45c910e1827db3eb9bd1310668ded
EderSouza93 <ederaldo.souza93@gmail.com>
2022-06-27 10:26:28 -0300

    Aulas 4 e 5

---
+++ JavaScript/Sintaxe_Basica/Aula_05 - Funções/script.js
@@ -0,0 +1,11 @@
+/* function Funcao (){
+    console.log('Essa mensagem veio de uma função');  
+}
+
+Funcao(); */
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+} 
+
+mensagem('Tudo certo', 'Jovem!');
